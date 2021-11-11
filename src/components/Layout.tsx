942795e4b33af7520369143409928b8b22c04523
Slava Bezgachev <vyacheslav.bezgachev@isate.kz>
2021-11-11 18:22:40 +0600

    Fixed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,43 +6,6 @@ import Image from "next/image";
 const Layout: FC = ({ children }) => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Сказка - Новогодние подарки и кульки 2022</title>
-        <meta
-          name="description"
-          content="Детские новогодние подарки, новогодние кульки со сладостями в г. Кокшетау, г. Костанай и г. Петропавловск. Новогодние кульки с конфетами и шоколадом из Казахстана и России"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://skazka-podarki.kz/" />
-        <meta
-          property="og:title"
-          content="Сказка - Новогодние подарки и кульки 2022"
-        />
-        <meta
-          property="og:description"
-          content="Детские новогодние подарки, новогодние кульки со сладостями в г. Кокшетау, г. Костанай и г. Петропавловск. Новогодние кульки с конфетами и шоколадом из Казахстана и России"
-        />
-        <meta
-          property="og:image"
-          content="https://skazka-podarki.kz/share_image_wide.png"
-        />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://skazka-podarki.kz/" />
-        <meta
-          property="twitter:title"
-          content="Сказка - Новогодние подарки и кульки 2021"
-        />
-        <meta
-          property="twitter:description"
-          content="Детские новогодние подарки, новогодние кульки со сладостями в г. Кокшетау, г. Костанай и г. Петропавловск. Новогодние кульки с конфетами и шоколадом из Казахстана и России"
-        />
-        <meta
-          property="twitter:image"
-          content="https://skazka-podarki.kz/share_image_wide.png"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <header className={styles.header}>
         <div className={styles.headerTop}>
           <img src="/logo.png" alt="Логотип Сказка" />
